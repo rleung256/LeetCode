91a7f74219cb468a8d966a105fb75f85b5ef1574
Ryan Leung <rleung256@gmail.com>
2023-02-05 12:13:51 -0600

    Minor: format challenges

--- Easy/1-TwoSum.js
+++ Easy/1-TwoSum.js
@@ -5,33 +5,35 @@
  */
 
 // essentially this is a math problem using the 2s compliment
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     let ansArr = []
 
-    for(let i  = 0; i < nums.length; i++){
+    for (let i = 0; i < nums.length; i++) {
         const compliment = target - nums[i];
-        if(compliment in ansArr) {
-            console.log([ansArr[compliment],i])
+        if (compliment in ansArr) {
+            console.log([ansArr[compliment], i])
         }
-        
-    ansArr[nums[i]] = i
+
+        ansArr[nums[i]] = i
     }
 
 
 };
 
 
-twoSum([11,15,2,7], 9)
+twoSum([11, 15, 2, 7], 9)
 
 
 //alternative using an actual map seems like this is the DS to use
 
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     var map = new Map();
-  
-    for(var i = 0; i<nums.length; i++) {
+
+    for (var i = 0; i < nums.length; i++) {
         var num = nums[i];
-        if(map.get(num) === undefined) map.set(target-num, i);
+        if (map.get(num) === undefined) {
+            map.set(target - num, i);
+        }
         else return [map.get(num), i];
     }
-  };
+};
--- Medium/542-01Matix.js
+++ Medium/542-01Matix.js
@@ -1,18 +1,18 @@
 // solution credit : https://www.youtube.com/watch?v=CTqBOiciqc4
 
-const updateMatrix = function(mat) {
+const updateMatrix = function (mat) {
     // let mat = [[0,0,0],[0,1,0],[1,1,1]];
-    let dirs  = [[0,-1], [0,1],[1,0], [-1,0]];
+    let dirs = [[0, -1], [0, 1], [1, 0], [-1, 0]];
     let queue = [];
 
-    for(let i = 0; i < mat.length; i++){
-        for ( let j = 0; j <mat[0].length; j++){
-          if(mat[i][j] === 0){
-            queue.push([i,j,0]); // third param is distance
-          }  
-          else {
-            mat[i][j] = Infinity; // all non-zero nums are set to a huge value to check to see if count is less than this, in a huge grid distance could be v large
-          }
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[0].length; j++) {
+            if (mat[i][j] === 0) {
+                queue.push([i, j, 0]); // third param is distance
+            }
+            else {
+                mat[i][j] = Infinity; // all non-zero nums are set to a huge value to check to see if count is less than this, in a huge grid distance could be v large
+            }
         }
     }
 
@@ -21,17 +21,17 @@ const updateMatrix = function(mat) {
         let [currX, currY, dist] = queue.shift(); // a fancy way of declaring 3 vars from the position de-queued
 
         if (mat[currX][currY] > dist) {
-            mat[currX][currY]  = dist;
+            mat[currX][currY] = dist;
         }
 
-        for(let [x,y] of dirs){
-            let [nextX, nextY, nextVal] = [currX+x, currY+y, dist +1]
-        
-            if(nextX <  0 || nextX  > mat.length - 1 || nextY < 0 || nextY > mat[0].length-1){ // check out of bounds
+        for (let [x, y] of dirs) {
+            let [nextX, nextY, nextVal] = [currX + x, currY + y, dist + 1]
+
+            if (nextX < 0 || nextX > mat.length - 1 || nextY < 0 || nextY > mat[0].length - 1) { // check out of bounds
                 continue;
             }
 
-            if(mat[nextX][nextY] === Infinity) { // found a 1 ..perform bfs on this element by pushing on to q
+            if (mat[nextX][nextY] === Infinity) { // found a 1 ..perform bfs on this element by pushing on to q
                 queue.push([nextX, nextY, nextVal])
             }
         }
@@ -40,4 +40,4 @@ const updateMatrix = function(mat) {
     return mat
 }
 
-updateMatrix([[0,0,0],[0,1,0],[1,1,1]]);
+updateMatrix([[0, 0, 0], [0, 1, 0], [1, 1, 1]]);
