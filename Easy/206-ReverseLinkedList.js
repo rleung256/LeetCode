cb905c9ba94dd5e8b69a163040d8babfd5a999f5
Ryan Leung <rleung256@gmail.com>
2023-02-10 10:40:53 -0600

    Challenge:206-ReverseLinnkedList

---
+++ Easy/206-ReverseLinkedList.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * 
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * 
+ * think of this one as just reversing the pointers one at a time and its super easy
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
+function reverseList(head) {
+    var prev = null; // think of this as a pointer and head is our other pointer
+    while (head) {
+
+        var next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+
+    }
+    return prev;
+}
+
+reverseList([1, 2, 3, 4, 5])
+// [5,4,3,2,1]
+
+// null=>1=>2=>3=>4=>5 set next to head.next
+// prev  h  n
+
+// null<=1=>2=>3=>4=>5 head.next pointer now looks at prev
+// prev  h  n
+
+// null<=1=>2=>3=>4=>5 move head over(set head to next)
+// prev     h
+
+// null<=1=>2=>3=>4=>5 set next to head.next
+// prev     h  n
+
+// null<=1<=2=>3=>4=>5 set head.next to prev
+//       p  h  n
+
+// null<=1<=2=>3=>4=>5 set head to next
+//       p     h
+
+// null<=1<=2=>3=>4=>5 set next to head.next
+//       p     h  n
+
+// null<=1<=2<=3=>4=>5 set head.next to prev  and so on..
+//          p  h  n
