3a40d244f7e4b3e77c62c14df2a58c456ff4a64d
Ryan Leung <rleung256@gmail.com>
2023-02-21 14:57:34 -0600

    Update: 49 - GroupAnagrams

--- Medium/49-GroupAnagrams.js
+++ Medium/49-GroupAnagrams.js
@@ -29,3 +29,27 @@ var groupAnagrams = function (strs) {
 
 groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
 //[["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+var groupAnagrams = function (strs) {
+
+    let sorted = strs.map((str) => str.split('').sort().join(''))
+
+    let map = {};
+
+    for (let i = 0; i < sorted.length; i++) {
+
+        if (!map[sorted[i]]) {
+            map[sorted[i]] = [strs[i]];
+        }
+
+        else {
+            map[sorted[i]].push(strs[i])
+
+
+        }
+
+
+    }
+    return Object.values(map)
+}
